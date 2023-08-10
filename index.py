487bddcecac3e4c7177dfeed1de74353c018f0f5
Mihai Aperghis <mihai.aperghis@vertify.ro>
2023-08-10 03:10:19 +0300

    move docs to README

--- index.py
+++ index.py
@@ -9,24 +9,6 @@ from google.cloud import iam
 import logging
 
 def get_credentials():
-    """
-    Obtain Google API credentials for impersonating a specific user via a service account with domain-wide delegation, 
-    without using any service account keys or JSON files. The code assumes that it will be executed by a Google Cloud
-    Function, Google Cloud Run, or Google Compute Engine instance with a service account attached to it that is different
-    from the service account that will be used for impersonation. Feel free to modify the code to suit your needs.
-    
-    Prerequisites:
-        - Enable domain-wide delegation for a new or existing service account. This script assumes that the service
-        account with domain-wide delegation is different from the service account executing this code.
-        - Enable the "IAM Service Account Credentials API" for the project, as well as any APIs that you will be accessing.
-        - Grant the "Service Account Token Creator" role to the service account executing this code.
-        - Set up environment variables: SERVICE_ACCOUNT_EMAIL, USER_EMAIL_TO_IMPERSONATE, GOOGLE_API_SCOPES (comma-separated
-        list of scopes).
-    
-    Returns:
-        google.oauth2.credentials.Credentials: Google API credentials for the impersonated user.
-    """
-    
     # Step 1: Set up the environment
     service_email = os.environ.get("SERVICE_ACCOUNT_EMAIL")
     user_email = os.environ.get("USER_EMAIL_TO_IMPERSONATE")
@@ -35,7 +17,7 @@ def get_credentials():
     # Step 2: Get IAM Credentials for the service account that is executing this code
     creds_iam, project = google.auth.default(scopes=["https://www.googleapis.com/auth/iam"])
 
-    # Step 3: Constructing the JWT (JSON Web Token)
+    # Step 3: Construct the JWT (JSON Web Token)
     payload = {
         "aud": "https://oauth2.googleapis.com/token",
         "exp": int(time.time()) + 3600,
@@ -63,7 +45,7 @@ def get_credentials():
         "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
         "assertion": signed_jwt,
     }
-    response = requests.post(token_url, data=token_data, timeout=10)
+    response = requests.post(token_url, data=token_data)
 
     # Step 6: Return the impersonated user Credentials
     if response.status_code == 200:
